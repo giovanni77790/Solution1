8fd7cc4b87f9b64c79f831833ac79e2ef1684e2d
Giovanni77790 <giovanniarcangioli1@gmail.com>
2020-11-11 21:44:44 +0100

    peso di un immagine

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,38 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int calcolo;
+            Console.WriteLine("inserisci il primo valore:");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il secondo valore:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inserisci il numero di bit:");
+            int risposta = Convert.ToInt32(Console.ReadLine());
+
+            if (risposta == 1)
+            {
+                calcolo = ((n1 * n2) / 8) / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if ( risposta == 8)
+            {
+                calcolo = (n1 * n2) / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if ( risposta == 16)
+            {
+                calcolo = (n1 * n2) * 2 / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else if ( risposta == 24)
+            {
+                calcolo = (n1 * n2) * 3 / 1024;
+                Console.WriteLine($"L'immagine pesa {calcolo}KB");
+            }
+            else
+            {
+                Console.WriteLine("vale solo per 1, 8, 16, 24 bit");
+            }
         }
     }
 }
